15cc23abada6ccd2c551a6da7a612c514156602c
stevenzhang <stevenzhang904@gmail.com>
2026-02-14 16:29:46 -0700

    Initial Commit

---
+++ CountDown_ProMax/CountDown_ProMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDown_ProMaxApp.swift
+//  CountDown_ProMax
+//
+//  Created by STEVEN ZHANG on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CountDown_ProMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
